631e17671d6421e7d2cc3672d6283756452500e7
Elisha-Sani <elishamuuo1@gmail.com>
2024-01-03 03:23:34 -0800

    Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check the singly linked list if it has a cycle
+ * @list: points the head of the linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listsint_t *sl = list, *ft = list;
+
+	while (sl && ft && ft->next)
+	{
+		sl = sl->next;
+		ft = ft->next->next;
+
+		if (sl == ft)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
