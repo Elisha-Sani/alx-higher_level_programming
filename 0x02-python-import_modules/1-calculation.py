2d527ea6987353849ad4a70c067bbbd2bf60067b
Elisha-Sani <elishamuuo1@gmail.com>
2024-01-13 00:52:32 -0800

    My first toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,6 @@ a = 10
 b = 5
 
 if __name__ == "__main__":
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} + {b} = {sub(a, b)}")
-    print(f"{a} + {b} = {mul(a, b)}")
-    print(f"{a} + {b} = {div(a, b)}")
+    operations = [("+", add(a, b)), ("-", sub(a, b)), ("*", mul(a, b)), ("/", div(a, b))]
+    for op, result in operations:
+        print(f"{a} {op} {b} = {result}")
