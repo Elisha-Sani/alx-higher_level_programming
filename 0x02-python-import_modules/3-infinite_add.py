adee4fd42ce1f6cf60c2d4f6eb46f7a732901429
Elisha-Sani <elishamuuo1@gmail.com>
2024-01-16 00:17:25 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 import sys
 
-def main():
+if __name__ == "__main__":
     #convert arguments to integers and calc
     sum_args = sum(int(arg) for arg in sys.argv[1:])
     print(sum_args)
-
-if __name__ == "__main__":
-    main()
